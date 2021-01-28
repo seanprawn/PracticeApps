052bd89352c4ee6dd3739cc5b0fce9e8e24ea8be
Sean Liebenberg <seansound@gmail.com>
2021-01-28 22:52:04 +0200

    COmpleted adding layout for tabs and fragments

---
+++ TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tabexperiment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /* TODO
+    * Android fundamentals 04.4: User navigation
+    * --> 2.3 Add a PagerAdapter
+    *
+     * */
+}
