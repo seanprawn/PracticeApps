82dff537944531dce55112510f9600c6a91ff6a1
Sean Liebenberg <seansound@gmail.com>
2021-02-01 22:07:55 +0200

    Finished styling, still busy with homework battery app

--- Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.scorekeeper;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private int mScore2;
     private TextView mScoreText1;
     private TextView mScoreText2;
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         mScoreText1 = (TextView)findViewById(R.id.score_1);
         mScoreText2 = (TextView)findViewById(R.id.score_2);
 
+        if (savedInstanceState != null)
+        {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+
+        }
     }
 
     public void decreaseScore(View view) {
@@ -55,6 +71,47 @@ public class MainActivity extends AppCompatActivity {
     }
     /* TODO
     *   Android fundamentals 05.1: Drawables, styles, and themes
-     * 4.1 Explore themes
+     * 
+     * **************************************************************
+     * Challenge 1 - StateList Drawable buttons! ---- Still to do Sean!
+     * ***************************************************************
      * */
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.night_mode)
+        {
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES)
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }else
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Recreate the activity for the theme change to take effect.
+            recreate();
+        }
+            return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the scores.
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;    }
 }
