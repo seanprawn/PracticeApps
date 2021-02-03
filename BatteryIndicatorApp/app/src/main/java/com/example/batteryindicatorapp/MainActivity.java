ad8c129c61f8e86c1917cdc0580dd6f15b6b0d2b
Sean Liebenberg <>
2021-02-03 08:34:55 +0200

    fINISHED HOMEWORK FOR bATTERY Indicator app

--- BatteryIndicatorApp/app/src/main/java/com/example/batteryindicatorapp/MainActivity.java
+++ BatteryIndicatorApp/app/src/main/java/com/example/batteryindicatorapp/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.batteryindicatorapp;
 
+import android.graphics.drawable.LevelListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    LevelListDrawable levelList;
+    public int batLevel = 0;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.ic_battery_level_list);
+        levelList = (LevelListDrawable) imageView.getBackground();
+    }
+
+    public void downClick(View view) {
+        if (batLevel > 0) {
+            batLevel--;
+            imageView.setImageLevel(batLevel);
+        }
+
+    }
+
+    public void upClick(View view) {
+        if (batLevel >= 0 && batLevel <= 9)
+        {
+            batLevel++;
+            if (batLevel ==1) batLevel++;
+            imageView.setImageLevel(batLevel);
+        }
     }
 }
