c1cbf95b56c3cbdbf762d98a8f2983e3693e2063
Sean Liebenberg <seansound@gmail.com>
2021-02-10 21:29:52 +0200

    Completed 4. Task 2: Update or cancel the notification, 2.3 Toggle the button state

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
@@ -1,20 +1,152 @@
 package com.example.notifyme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
+
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0; // associate the notification with a notification ID
+
+
+    /*
+    Every notification channel must be associated with an ID that is unique within your package.
+    You use this channel ID later, to post your notifications.
+*/
+private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+        createNotificationChannel();// Do not forget else app crashes!
+
+        button_update = findViewById(R.id.update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Update the notification
+                 updateNotification();
+            }
+        });
+
+        button_cancel = findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Cancel the notification
+                cancelNotification();
+            }
+        });
+        setNotificationButtonState(true, false, false);
+
     }
+
+    public void updateNotification() {
+        Bitmap androidImage = BitmapFactory
+                .decodeResource(getResources(),R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage)
+                .setBigContentTitle("Notification Updated!"));
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        setNotificationButtonState(false, false, true);
+    }
+    public void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+        setNotificationButtonState(true, false, false);
+    }
+
+    public void createNotificationChannel()
+    {
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+//
+        //      notification channels are only available in API 26 and higher,
+        //      add a condition to check for the device's API version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create a NotificationChannel
+//     The name is displayed under notification Categories in the device's user-visible Settings app
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager
+                    .IMPORTANCE_HIGH);
+
+//            configure the notificationChannel object's initial settings
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder()
+    {
+        Intent notificationIntent = new Intent(this, MainActivity.class); // launches MainActivity when notification is clicked
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, //communicate with another app
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_android)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setContentIntent(notificationPendingIntent) //Sets the content intent
+                .setAutoCancel(true); //closes the notification when user taps on it.
+
+
+
+        return notifyBuilder;
+    };
+
+
+    public void sendNotification() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        setNotificationButtonState(false, true, true);
+    }
+
+    void setNotificationButtonState(Boolean isNotifyEnabled,
+                                    Boolean isUpdateEnabled,
+                                    Boolean isCancelEnabled) {
+        button_notify.setEnabled(isNotifyEnabled);
+        button_update.setEnabled(isUpdateEnabled);
+        button_cancel.setEnabled(isCancelEnabled);
+    }
+
     /* TODO
     * Android fundamentals 08.1: Notifications
-    * 1.1 Create the project
-    * https://developer.android.com/codelabs/android-training-notifications?index=..%2F..%2Fandroid-training#2
-    * 
+    * 5. Task 3: Add a notification action button
+    * https://developer.android.com/codelabs/android-training-notifications?index=..%2F..%2Fandroid-training#4
     * */
 }
