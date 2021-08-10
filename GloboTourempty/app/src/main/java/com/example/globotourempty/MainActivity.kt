dc423583b55ab0458199b47cefb490f6cf6d0100
Sean Liebenberg <>
2021-08-10 12:59:13 +0200

    Added excercise files for recyclerViewNavigationDrawer

--- GloboTourempty/app/src/main/java/com/example/globotourempty/MainActivity.kt
+++ GloboTourempty/app/src/main/java/com/example/globotourempty/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.globotourempty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar        : MaterialToolbar
     private lateinit var navController  : NavController
+    private lateinit var navigationView : NavigationView
+    private lateinit var drawerlayout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Views
         toolbar         = findViewById(R.id.activity_main_toolbar)
+        navigationView = findViewById(R.id.nav_controller_view_tag)
+        drawerlayout = findViewById(R.id.drawer_layout)
 
         // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
